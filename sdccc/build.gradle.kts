24c3548542a4dcff2ab43407967ae4a5ae56673b
midttuna <augustmagnus.midttun@draeger.com>
2024-12-03 10:26:56 +0200

    print permissions gradle

--- sdccc/build.gradle.kts
+++ sdccc/build.gradle.kts
@@ -136,14 +136,13 @@ tasks.register<Copy>("unpackJre") {
     dependsOn("downloadJre")
     from(zipTree(file("${layout.buildDirectory.get().asFile}/${jreBasePath}/${jreDownloadFileName}")))
     into("${layout.buildDirectory.get().asFile}/${jreBasePath}")
-    doLast {
+    doFirst {
         file("${layout.buildDirectory.get().asFile}/${jreBasePath}").walk().forEach { file ->
-            if (!file.setWritable(true)) {
-                println("Failed to set writable permission for ${file.absolutePath}")
-            }
+            println("File: ${file.absolutePath}, Writable: ${file.canWrite()}, Readable: ${file.canRead()}")
         }
     }
 }
+
 
 tasks.register("downloadAndUnpackJre") {
     dependsOn("unpackJre")
